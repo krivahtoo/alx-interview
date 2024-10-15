471801df880bec1f26379965e54b9d97bdd2536d
Noah Too <krivahtoo@gmail.com>
2024-10-15 06:45:38 +0300

    Add 0x01. Lockboxes task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+0. Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+    """
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+
+    return all(unlocked)
+
+
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
